c850d34e327a501f51d31195e387c00f3e59989f
Chris Duncan <cduncan418@gmail.com>
2026-05-02 23:59:07 -0400

    update primary domain to cduncanx.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://duncanc.io',
+  site: 'https://cduncanx.com',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
